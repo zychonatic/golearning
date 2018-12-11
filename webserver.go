c13b2eefbebc218ab7edb6693a672a0f79ad3907
Matthias Zeilinger <Matthias.Zeilinger@willhaben.at>
2018-12-11 13:57:03 +0100

    changed to gorilla/mux httpserver

--- webserver.go
+++ webserver.go
@@ -4,18 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 func createHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I will create %s!", r.URL.Path[len("/create/"):])
+	params := mux.Vars(r)
+	fmt.Fprintf(w, "Hi there, I will create %s!", params["id"])
 }
 
 func deleteHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "this will delete %s", r.URL.Path[len("/delete/"):])
+	params := mux.Vars(r)
+	fmt.Fprintf(w, "this will delete %s", params["id"])
 }
 
 func main() {
-	http.HandleFunc("/create/", createHandler)
-	http.HandleFunc("/delete/", deleteHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter()
+	router.HandleFunc("/test/{id}", createHandler).Methods("POST")
+	router.HandleFunc("/test/{id}", deleteHandler).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
