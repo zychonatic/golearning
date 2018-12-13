a2bafb49e7a16c05611ff1aa0d5da97792a5bc6a
Matthias Zeilinger <Matthias.Zeilinger@willhaben.at>
2018-12-13 08:36:22 +0100

    added elasticsearch get indices endpoint

--- webserver.go
+++ webserver.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"io/ioutil"
 	"github.com/gorilla/mux"
 )
 
@@ -17,8 +18,20 @@ func deleteHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "this will delete %s", params["id"])
 }
 
+func getHandler(w http.ResponseWriter, r *http.Request){
+	url := "http://localhost:9200/_cat/indices"
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	fmt.Fprintf(w, string(content))
+}
+
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/get", getHandler).Methods("GET")
 	router.HandleFunc("/test/{id}", createHandler).Methods("POST")
 	router.HandleFunc("/test/{id}", deleteHandler).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
