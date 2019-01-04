6279ec7d31245639e9323f6608f9f1571f4d4865
Matthias Zeilinger <Matthias.Zeilinger@willhaben.at>
2019-01-04 10:33:00 +0100

    different command added

--- exec.go
+++ exec.go
@@ -13,7 +13,17 @@ func main() {
 
     // Pipe a directory listing through sort.
 	cmd1 := exec.Command("ls", "-la")
+	args := []string{"-la","*.go"}
+	cmd3 := exec.Command("ls", args...)
 	cmd2 := exec.Command("grep", "go")
+
+	test, err := cmd3.CombinedOutput()
+	scanner2 := bufio.NewScanner(strings.NewReader(string(test)))
+	for scanner2.Scan() {
+		re := regexp.MustCompile(" +")
+                s := re.Split(scanner2.Text(),-1)
+                fmt.Println(s[8])
+	}
 
     // Get the pipe of Stdout from cmd1 and assign it
     // to the Stdin of cmd2.
